76e96ed0052f7be801311c7ef45c335a08eee769
SteveMustanski <steve.mustanski@gmail.com>
2019-04-12 13:13:07 -0400

    added router for api, set up books route, and created seed file and instructions for the database

---
+++ booksJson.js
@@ -0,0 +1,23 @@
+db.books.insert([
+  {
+    title: "Hitchiker's Guide to the Galaxy",
+    genre: 'Science Fiction',
+    author: 'Douglas Adams',
+    bookId: 386162,
+    read: false,
+  },
+  {
+    title: 'The Lord of the Rings',
+    genre: 'Fantasy',
+    author: 'JRR Tolkien',
+    bookId: 33,
+    read: false,
+  },
+  {
+    title: 'Atlas Shrugged',
+    genre: 'Fiction',
+    author: 'Ayn Rand',
+    bookId: 662,
+    read: false,
+  },
+]);
