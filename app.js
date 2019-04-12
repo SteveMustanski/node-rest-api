18086c7ab2288b7e6f8cea6dc183e220ae0a1d9d
SteveMustanski <steve.mustanski@gmail.com>
2019-04-12 17:13:53 -0400

    added post books and body-parser package

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,19 +10,30 @@ const db = mongoose.connect('mongodb://localhost/bookAPI', {
 const bookRouter = express.Router();
 const Book = require('./models/bookModel');
 
-bookRouter.route('/books').get((req, res) => {
-  //get the query from the url and pass it to the find
-  const query = {};
-  if (req.query.genre) {
-    query.genre = req.query.genre;
-  }
-  Book.find(query, (err, books) => {
-    if (err) {
-      return res.send(err);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+bookRouter
+  .route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+
+    book.save();
+    res.status(201).json(book);
+  })
+  .get((req, res) => {
+    //get the query from the url and pass it to the find
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
     }
-    return res.json(books);
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
   });
-});
 bookRouter.route('/books/:dbBookId').get((req, res) => {
   //get the db book id from the url and pass it to the find
   Book.findById(req.params.dbBookId, (err, book) => {
