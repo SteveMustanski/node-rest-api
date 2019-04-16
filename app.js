f27e120e982bca2882725445cc947fca4203fda2
SteveMustanski <steve.mustanski@gmail.com>
2019-04-16 12:22:00 -0400

    added code to run in test mode to app

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const db = mongoose.connect('mongodb://localhost/bookAPI', {
-  useNewUrlParser: true,
-});
+if (process.env.ENV === 'Test') {
+  console.log('Running in test mode');
+  const db = mongoose.connect('mongodb://localhost/bookAPI_Test', {
+    useNewUrlParser: true,
+  });
+} else {
+  console.log('This is not a test');
+  const db = mongoose.connect('mongodb://localhost/bookAPI_prod', {
+    useNewUrlParser: true,
+  });
+}
 const Book = require('./models/bookModel');
 const bookRouter = require('./routes/bookRouter')(Book);
 
